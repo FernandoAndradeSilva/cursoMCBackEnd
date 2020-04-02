4284bd2e571653fe4e3f4581d273148cea3f6348
Nando <fernando.andrade.inf@gmail.com>
2020-04-02 13:42:42 -0300

    implementacao modelo conceitual OK

---
+++ src/main/java/com/fernando/cursomc/services/CategoriaService.java
@@ -0,0 +1,26 @@
+package com.fernando.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fernando.cursomc.domain.Categoria;
+import com.fernando.cursomc.repositories.CategoriaRepository;
+import com.fernando.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
+	}
+
+}
